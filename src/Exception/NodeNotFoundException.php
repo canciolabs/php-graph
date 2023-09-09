72b6a45546ad8f13d982a6d949151235d8a9e5c5
Nilo Soares <nilosoares@hotmail.com>
2023-09-09 12:51:21 -0300

    Add a constructor to the NodeNotFoundException

--- src/Exception/NodeNotFoundException.php
+++ src/Exception/NodeNotFoundException.php
@@ -2,9 +2,18 @@
 
 namespace Cancio\Graph\Exception;
 
+use Cancio\Graph\Node\NodeInterface;
 use Exception;
+use Throwable;
 
 class NodeNotFoundException extends Exception
 {
+
+    public function __construct(NodeInterface $node, $code = 0, Throwable $previous = null)
+    {
+        $message = sprintf('The node "%s" was not found.', $node->getId());
+
+        parent::__construct($message, $code, $previous);
+    }
 
 }
