80bd85d8d3f9c03961ea8446dfb6e38b19818129
Nilo Soares <nilosoares@hotmail.com>
2023-09-10 08:46:08 -0300

    Add the AbstractDFS class

---
+++ tests/Algorithm/AbstractDFSTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cancio\Graph\Tests\Algorithm;
+
+use Cancio\Graph\AdjacencyMatrix;
+use Cancio\Graph\Algorithm\AbstractDFS;
+use Cancio\Graph\Algorithm\TopologicalSort;
+use Cancio\Graph\Edge\Edge;
+use Cancio\Graph\GraphInterface;
+use Cancio\Graph\Node\Node;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class AbstractDFSTest extends TestCase
+{
+
+    public function testGetGraph(): void
+    {
+        $graph = $this->getGraph();
+
+        $algo = new TopologicalSort($graph);
+
+        $this->assertSame($graph, $algo->getGraph());
+    }
+
+    protected function getGraph(): GraphInterface
+    {
+        $a = new Node('A');
+        $b = new Node('B');
+        $c = new Node('C');
+        $nodes = [$a, $b, $c];
+
+        $edge1 = new Edge($a, $b);
+        $edge2 = new Edge($a, $c);
+        $edges = [$edge1, $edge2];
+
+        return new AdjacencyMatrix($nodes, $edges);
+    }
+
+    protected function getAbstractDFSInstance(GraphInterface $graph): MockObject
+    {
+        return $this->getMockForAbstractClass(AbstractDFS::class, $graph);
+    }
+
+}
